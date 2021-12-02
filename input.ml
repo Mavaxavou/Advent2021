cfc02692debcd0f1d77b6c084c39f06ef9ad12bf
Maxime Jacquemin <maxime.jacquemin@cea.fr>
2021-12-02 11:25:49 +0100

    Using the standard library for reading lines

--- input.ml
+++ input.ml
@@ -1,8 +1,4 @@
-let read_lines path =
-  let ch = open_in path in
-  let rec aux acc = try aux (input_line ch :: acc) with End_of_file -> acc in
-  let result = List.rev (aux []) in
-  close_in ch ; result
+let read_lines path = Arg.read_arg path |> Array.to_list
 
 let convert_data (f : 'a -> 'b option) (data : 'a list) : 'b list option =
   try Some (List.map (fun elt -> f elt |> Option.get) data)
