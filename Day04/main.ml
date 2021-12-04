56f7980a60ef1d348069eb167d3a9e1e33e09dde
Maxime Jacquemin <maxime2.jacquemin@gmail.com>
2021-12-05 00:17:44 +0100

    Day04 / Part1
    That's quite overkill but it was fun

---
+++ Day04/main.ml
@@ -0,0 +1,124 @@
+open Utils
+
+
+
+module Data = struct
+
+  module M = Map.Make(Int)
+  type 'n t = 'n Matrix.pos list M.t
+
+  let empty = M.empty
+
+  let find (data : int) (m : 'n t) : 'n Matrix.pos list option =
+    M.find_opt data m
+
+  let add (data : int) (p : 'n Matrix.pos) (m : 'n t) : 'n t =
+    match M.find_opt data m with
+    | None -> M.add data [p] m
+    | Some ps -> M.add data (p :: ps) m
+
+end
+
+
+
+type 'n bingo =
+  { matrix : (int * bool, 'n) Matrix.t
+  ; map : 'n Data.t
+  ; lines : ('n succ Finite.t, 'n) Vect.t
+  ; columns : ('n succ Finite.t, 'n) Vect.t
+  }
+
+let create (matrix : (int, 'n) Matrix.t) : 'n bingo =
+  let length = Vect.length matrix in
+  let counter = Finite.of_nat length in
+  let lines = Vect.make length counter in
+  let columns = Vect.make length counter in
+  let map = Matrix.fold (fun p n -> Data.add n p) matrix Data.empty in
+  let matrix = Matrix.map (fun i -> (i, false)) matrix in
+  { matrix ; map ; lines ; columns }
+
+
+
+type 'n result =
+  | Unfinished of 'n bingo
+  | LineCompleted of 'n Finite.t * (int * bool, 'n) Matrix.t
+  | ColumnCompleted of 'n Finite.t * (int * bool, 'n) Matrix.t
+
+let stamp_a_pos (type n) (pos : n Matrix.pos) (bingo : n bingo) : n result =
+  let decrease n = Finite.decrease_not_zero_exn n in
+  let decrease i vs = try Ok (Vect.edit decrease i vs) with _ -> Error i in
+  let lines = decrease pos.Matrix.line bingo.lines in
+  let columns = decrease pos.Matrix.column bingo.columns in
+  let matrix = Matrix.edit (fun (n, _) -> (n, true)) pos bingo.matrix in
+  match lines, columns with
+  | Error line, _ -> LineCompleted (line, matrix)
+  | _, Error column -> ColumnCompleted (column, matrix)
+  | Ok lines, Ok columns ->
+    Unfinished { bingo with matrix ; lines ; columns }
+
+let mark (type n) (number : int) (bingo : n bingo) : n result =
+  match Data.find number bingo.map with
+  | None -> Unfinished bingo
+  | Some ps ->
+    let stamp r p = match r with Unfinished b -> stamp_a_pos p b | _ -> r in
+    List.fold_left stamp (Unfinished bingo) ps
+
+let rec play (numbers : int list) (bingos : 'n bingo list) =
+  match numbers with
+  | [] -> Format.printf "No winner...@."
+  | n :: ns ->
+    let module E = struct exception Winner of int end in
+    let round_results = List.map (mark n) bingos in
+    let is_their_a_winner index = function
+      | Unfinished bingo -> bingo
+      | LineCompleted (_, matrix) | ColumnCompleted (_, matrix) ->
+        let f _ (v, b) acc = if not b then v + acc else acc in 
+        let score = n * Matrix.fold f matrix 0 in
+        raise (E.Winner score)
+    in
+    try play ns (List.mapi is_their_a_winner round_results)
+    with E.Winner score -> Format.printf "Final score : %d@." score
+
+
+
+type 'n data_read = Sep | Row of (int, 'n) Vect.t
+
+let read_bingos (size : 'n nat) (line : string) : 'n data_read option =
+  let not_empty s = String.length s != 0 in
+  let split line = String.split_on_char ' ' line |> (List.filter not_empty) in
+  let row vs = Row (Vect.map int_of_string vs) in
+  if String.length line = 0 then Some Sep
+  else Vect.of_list (split line) size |> Option.map row
+
+let rec pack : type m n. n nat -> m nat -> (int, n) Matrix.t list ->
+  ((int, n) Vect.t, m) Vect.t -> n data_read list ->
+  (int, n) Matrix.t list option =
+    fun size read_lines result current_batch data ->
+      match dec_eq size read_lines, data with
+      | None, [] -> None
+      | Some Eq, [] -> Some (current_batch :: result)
+      | None, Sep :: data -> None
+      | None, Row row :: data ->
+        pack size (Succ read_lines) result (row :: current_batch) data
+      | Some Eq, Sep :: data ->
+        pack size Zero (current_batch :: result) [] data
+      | Some Eq, Row row :: data -> None
+
+let read (size : 'n nat) (lines : string list) =
+  match lines with
+  | numbers :: _ :: bingos ->
+    let numbers = String.split_on_char ',' numbers |> List.map int_of_string in
+    Option.bind (Utils.convert_data (read_bingos size) bingos) @@ fun data ->
+      let matrixes = pack size Zero [] [] data in
+      let res matrixes = numbers, List.map create matrixes in
+      Option.map res matrixes
+  | _ -> None
+
+
+
+let () =
+  let lines = Utils.read_lines "Day04/data" in
+  let size = Succ (Succ (Succ (Succ (Succ Zero)))) in
+  match read size lines with
+  | None -> assert false
+  | Some (numbers, bingos) -> play numbers bingos
--- utils.ml
+++ utils.ml
@@ -7,3 +7,133 @@ let read_lines path = Arg.read_arg path |> Array.to_list
 let convert_data (f : 'a -> 'b option) (data : 'a list) : 'b list option =
   try Some (List.map (fun elt -> f elt |> Option.get) data)
   with Invalid_argument _ -> None
+
+
+
+type zero = Z
+type 'a succ = S : 'a -> 'a succ
+
+type 'n nat =
+  | Zero : zero nat
+  | Succ : 'a nat -> 'a succ nat
+
+let rec to_int : type n. n nat -> int = function
+  | Zero -> 0
+  | Succ n -> 1 + to_int n
+
+
+type ('a, 'b) eq = Eq : ('a, 'a) eq
+
+let rec dec_eq : type n m. n nat -> m nat -> (n, m) eq option = fun n m ->
+  match n, m with
+  | Zero, Zero -> Some Eq
+  | Zero, _ | _, Zero -> None
+  | Succ n, Succ m -> match dec_eq n m with None -> None | Some Eq -> Some Eq
+
+
+
+module Finite = struct
+
+  type 'n t =
+    | FZ : ('n succ) t
+    | FS : 'n t -> ('n succ) t
+
+  let rec of_nat : type n. n nat -> n succ t = function
+    | Zero -> FZ
+    | Succ n -> FS (of_nat n)
+
+  let rec cast : type n. n t -> n succ t = function
+    | FZ -> FZ
+    | FS f -> FS (cast f)
+
+  let decrease : 'n t -> 'n t option = function
+    | FZ -> None
+    | FS f -> Some (cast f)
+
+  exception IsZero
+  let decrease_not_zero_exn (f : 'n t) : 'n t =
+    match decrease f with
+    | None | Some FZ -> raise IsZero
+    | Some res -> res
+
+  let rec to_int : type n. n t -> int = function
+    | FZ -> 0
+    | FS f -> 1 + to_int f
+
+  let pretty fmt f = Format.pp_print_int fmt (to_int f)
+
+end
+
+
+
+module Vect = struct
+
+  type ('a, 'n) t =
+    | [] : ('a, zero) t
+    | (::) : 'a * ('a, 'n) t -> ('a, 'n succ) t
+
+  let rec of_list : type n. 'a list -> n nat -> ('a, n) t option = fun l n ->
+    match l, n with
+    | [], Zero -> Some []
+    | l :: ls, Succ n -> Option.map (fun ls -> l :: ls) (of_list ls n)
+    | _, _ -> None
+
+  let rec length : type n. ('a, n) t -> n nat = function
+    | [] -> Zero
+    | _ :: xs -> Succ (length xs)
+
+  let rec make : type n. n nat -> 'a -> ('a, n) t = fun n elt ->
+    match n with
+    | Zero -> []
+    | Succ n -> elt :: make n elt
+
+  type ('n, 'x, 'a) folder = 'n Finite.t -> 'x -> 'a -> 'a
+  let rec fold : type n. (n, 'x, 'a) folder -> ('x, n) t -> 'a -> 'a =
+    fun f xs acc ->
+      match xs with
+      | [] -> acc
+      | x :: xs -> fold (fun i -> f (FS i)) xs (f FZ x acc)
+
+  let rec map : type n. ('a -> 'b) -> ('a, n) t -> ('b, n) t = fun f -> function
+    | [] -> []
+    | x :: xs -> f x :: map f xs
+
+  let rec edit : type n. ('a -> 'a) -> n Finite.t -> ('a, n) t -> ('a, n) t =
+    fun f pos xs -> match pos, xs with
+      | FZ, x :: xs -> f x :: xs
+      | FS pos, x :: xs -> x :: edit f pos xs
+
+  let rec pretty : type n.
+    pp_sep: (Format.formatter -> unit -> unit) ->
+    (Format.formatter -> 'a -> unit) ->
+    Format.formatter -> ('a, n) t -> unit =
+      fun ~pp_sep f fmt -> function
+        | [] -> ()
+        | [x] -> f fmt x
+        | x :: y :: xs ->
+          Format.fprintf fmt "%a%a%a" f x pp_sep () (pretty ~pp_sep f) (y :: xs)
+
+end
+
+
+
+module Matrix = struct
+
+  type 'n pos = { line : 'n Finite.t ; column : 'n Finite.t }
+  type ('x, 'n) t = (('x, 'n) Vect.t, 'n) Vect.t
+
+  let fold : type n. (n pos -> 'x -> 'a -> 'a) -> ('x, n) t -> 'a -> 'a =
+    fun f -> Vect.(fold (fun line -> fold (fun column -> f {line ; column})))
+
+  let map f matrix = Vect.(map (map f) matrix)
+
+  let edit : type n. ('a -> 'a) -> n pos -> ('a, n) t -> ('a, n) t =
+    fun f pos matrix -> Vect.(edit (edit f pos.column) pos.line matrix)
+
+  let pretty f fmt matrix =
+    let pp_sep = Format.pp_print_space in
+    let pp_line fmt =
+      Format.fprintf fmt "@[<h>%a@]" (Vect.pretty ~pp_sep f)
+    in Format.fprintf fmt "@[<v>%a@]" (Vect.pretty ~pp_sep pp_line) matrix
+
+end
